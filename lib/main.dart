8cb42ff0fa3f4c5f6dac4febe07aee980b84006c
Bilal Alptekin <bilalalptekin362@gmail.com>
2021-10-04 12:42:33 +0300

    V:1.0.0

--- lib/main.dart
+++ lib/main.dart
@@ -146,9 +146,6 @@ class _MusicplayerState extends State<Musicplayer> {
               //       ),);
               //  },
               // ),
-             
-
-             
             ],
           ),
         ),
